57f861f2559792f0ea38445772677137cb9a52e6
Dapada <92043688+Dapada@users.noreply.github.com>
2022-02-27 11:17:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-// Get the modal
+
+document.addEventListener("DOMContentLoaded", () => {
+  const LoginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#createAccount");
+
+
+  document.querySelector("#linkCreateAccount").addEventListener(click, e => {
+    e.preventDefault();
+    LoginForm.classList.add("form--hidden");
+    createAccountForm.classList.remove("form--hidden");
+});
+
+
+document.querySelector("#linkLogin").addEventListener("click", e => {
+  e.preventDefault();
+   LoginForm.classList.remove("form--hidden");
+   createAccountForm.classList.add("form--hidden");
+});  
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// Get the modal
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
@@ -6,4 +39,4 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+}*/
